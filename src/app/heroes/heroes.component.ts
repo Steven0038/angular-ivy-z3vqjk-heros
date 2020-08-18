a0e42c330807c52aa975cf284b22f63e5c68ee15
Steven0038 <stevenchang0038@gmail.com>
2020-08-18 17:02:22 +0800

    nav and router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,10 +12,11 @@ import { MessageService } from '../message.service'
 export class HeroesComponent implements OnInit {
   // heroes = HEROES;
   heroes: Hero[];
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
   constructor(private heroService: HeroService,
-              private messageService: MessageService) { }
+              // private messageService: MessageService) { }
+              ) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -31,8 +32,8 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
+  // onSelect(hero: Hero):void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  // }
 }
